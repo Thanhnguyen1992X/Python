967127f9424e3e579d63bcf35e423733be3c8440
nguyen duc thanh <142642773+Thanhnguyen1992X@users.noreply.github.com>
2023-12-12 13:29:20 +0700

    Add files via upload

---
+++ ChatGPT.py
@@ -0,0 +1,22 @@
+import openai
+from openai import OpenAI
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key="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",
+)
+
+def chat_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+if __name__ == "__main__":
+        while True:
+            user_input = input("You :")
+            if user_input.lower() in ["quit", "exit", "bye"]:
+                break
+
+            response = chat_gpt(user_input)
+            print("Chatbot :" ,response)
